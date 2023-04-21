3d2e717616ab4dd0d359e1be283e4f3a5e02df9e
Vandana Nayak <vandana.nayak@novaders.com>
2023-04-21 17:18:00 +0530

    sidebar

--- apps/webApp/src/app/widgets/setting/setting.module.ts
+++ apps/webApp/src/app/widgets/setting/setting.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SettingComponent} from "./setting.component";
 import {WidgetFrameModule} from "../../../../../../common/ui-component/frames/widget-frame/widget-frame.module";
+import {SidebarComponent} from "../../../../../../common/ui-component/sidebar/sidebar.component";
 
 
 @NgModule({
-  declarations: [SettingComponent],
+  declarations: [SettingComponent, SidebarComponent],
   imports: [
     CommonModule,
     WidgetFrameModule
---
+++ common/ui-component/sidebar/sidebar-items/sidebar.ts
@@ -0,0 +1,46 @@
+export const sidebarItems = [
+  {
+    "type": "General",
+    "items": [
+      {
+        "icon": "",
+        "title": "Organization"
+      },
+      {
+        "icon": "",
+        "title": "Language and time"
+      },
+      {
+        "icon": "",
+        "title": "Disk Usage"
+      },
+      {
+        "icon": "",
+        "title": "Information"
+      }
+    ]
+  },
+  {
+    "type": "User settings",
+    "items": [{
+      "icon": "",
+      "title": "User"
+    }, {
+      "icon": "",
+      "title": "User Group"
+    }, {
+      "icon": "",
+      "title": "Permissions"
+    },]
+  },
+  {
+    "type": "Data Source"
+  },
+  {
+    "type": "Workspace"
+  },
+  {
+    "type": "Subscription",
+    "items": [{"icon": "", "title": "Payment"}, {"icon": "", "title": "Change Plan"}]
+  }
+]
