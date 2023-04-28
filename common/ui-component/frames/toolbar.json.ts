a99fbbbdd3dd61210df12d83135f1a5baeef8375
Vandana Nayak <vandana.nayak@novaders.com>
2023-04-28 17:45:16 +0530

    top toolbar styling

--- common/ui-component/frames/toolbar.json.ts
+++ common/ui-component/frames/toolbar.json.ts
@@ -1,9 +1,11 @@
 export const toolbar = [{
   "name": "Cymonix",
-  "url": "url",
+  "url": "../../../../assets/image/Logo2.png",
   "position": "top",
   "type": "image",
-  "sector": "1"
+  "sector": "1",
+  "height": "25",
+  "width": "85"
 }, {
   "name": "Dashboard",
   "position": "top",
@@ -20,17 +22,68 @@ export const toolbar = [{
     "position": "top",
     "type": "searchBox",
     "sector": "3"
-  },{
-    "name": "Save",
-    "position": "bottom",
-    "type": "button",
-    "class":"solid-button",
-    "sector": "2"
-  },{
+  }, {
     "name": "Cancel",
     "position": "bottom",
     "type": "button",
-    "class":"grey-button",
+    "class": "grey-button",
     "sector": "2"
-  }
+  }, {
+    "name": "Save",
+    "position": "bottom",
+    "type": "button",
+    "class": "solid-button",
+    "sector": "2"
+  },
+  {
+    "url": "../../../../assets/image/eye.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  }, {
+    "url": "../../../../assets/image/chat.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  }, {
+    "url": "../../../../assets/image/marketplace.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  }, {
+    "url": "../../../../assets/image/notifications.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  }, {
+    "url": "../../../../assets/image/settings.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  }, {
+    "url": "../../../../assets/image/user.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  }, {
+    "url": "../../../../assets/image/moreIcon2.svg",
+    "position": "top",
+    "type": "image",
+    "sector": "4",
+    "height": "15",
+    "width": "30"
+  },
+
 ]
