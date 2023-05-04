81845ab351bd5d8526d169b429be2b14e54a887c
Vandana Nayak <vandana.nayak@novaders.com>
2023-05-04 16:20:06 +0530

    Balloon Layout

--- common/ui-component/graph/graph.component.ts
+++ common/ui-component/graph/graph.component.ts
@@ -1,13 +1,20 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {
   Arrow,
-  ArrowType, BalloonLayout, DefaultLabelStyle,
+  ArrowType,
+  BalloonLayout,
+  DefaultLabelStyle,
   GraphBuilder,
-  GraphComponent, GraphOverviewComponent, IconLabelStyle, InteriorLabelModel,
-  License, Point,
+  GraphComponent,
+  GraphOverviewComponent,
+  IconLabelStyle,
+  InteriorLabelModel,
+  LayoutExecutor,
+  License,
   PolylineEdgeStyle,
   Rect,
-  ShapeNodeStyle, Size,
+  ShapeNodeStyle,
+  Size,
 } from "yfiles";
 import licenseValue from 'license.json';
 import {data} from "./data";
@@ -47,13 +54,14 @@ export class GraphComponents implements OnInit {
       stroke: null, fill: null //for no shape node
       // shape: 'ellipse'
     });
+    // set node size
+    nodesSource.nodeCreator.defaults.size = new Size(50, 50);
 
     //create text label
     const labelCreator = nodesSource.nodeCreator.createLabelBinding(data => data.label);
-    labelCreator.defaults.layoutParameter = InteriorLabelModel.SOUTH
-
+    labelCreator.defaults.layoutParameter = InteriorLabelModel.SOUTH;
     // create icon label
-    const iconCreator = nodesSource.nodeCreator.createLabelBinding()
+    const iconCreator = nodesSource.nodeCreator.createLabelBinding();
     // null check
     iconCreator.textProvider = node => (node.imageUrl != null ? '' : null)
     iconCreator.styleProvider = node =>
@@ -76,20 +84,28 @@ export class GraphComponents implements OnInit {
     edgesSource.edgeCreator.defaults.labels.style = new DefaultLabelStyle({
       backgroundFill: 'white'
     })
-
     //prepare the graph
     graphComponent.graph = builder.buildGraph();
 
-    //place the nodes
-    graphComponent.graph.nodes.forEach(node => {
-      // console.log(graphComponent.graph.nodes.get(7))
-      const x = data.nodes.find(n => n.id === node.tag.id)?.x;
-      const y = data.nodes.find(n => n.id === node.tag.id)?.y;
-      if (x !== undefined && y !== undefined) {
-        graphComponent.graph.setNodeLayout(node, new Rect(x, y, 50, 50))
-        const layout = node.layout
-      }
+    // create layout
+    const layout = new BalloonLayout({minimumNodeDistance:50});
+    const layoutExecutor = new LayoutExecutor({
+      graphComponent:graphComponent,
+      layout: layout,
+      duration: '0.5s',
+
     });
+    layoutExecutor.start().then();
+    //place the nodes
+    // graphComponent.graph.nodes.forEach(node => {
+    //   // console.log(graphComponent.graph.nodes.get(7))
+    //   const x = data.nodes.find(n => n.id === node.tag.id)?.x;
+    //   const y = data.nodes.find(n => n.id === node.tag.id)?.y;
+    //   if (x !== undefined && y !== undefined) {
+    //     graphComponent.graph.setNodeLayout(node, new Rect(x, y, 50, 50))
+    //     const layout = node.layout
+    //   }
+    // });
 
     // customise one node style
     const nodes = graphComponent.graph.nodes.find(n => n.tag.label === 'Akshay');
@@ -103,11 +119,16 @@ export class GraphComponents implements OnInit {
       graphComponent.graph.setStyle(nodes, nodeStyle)
     }
 
+    this.initializeOverviewComponent(graphComponent)
+
+  }
+
+
+  private initializeOverviewComponent(graphComponent: GraphComponent ) {
+
     const overviewComponent = new GraphOverviewComponent('#overview', graphComponent);
     overviewComponent.autoDrag = true;
     overviewComponent.contentRect = new Rect(0,0,1000,1000)
     overviewComponent.fitContent(true).then();
   }
-
-
 }
