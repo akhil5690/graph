69f5fed08076505f8d6ff47db7238ae1261488c2
Vandana Nayak <vandana.nayak@novaders.com>
2023-05-04 11:49:10 +0530

    overview component added

--- common/ui-component/graph/graph.component.ts
+++ common/ui-component/graph/graph.component.ts
@@ -3,8 +3,8 @@ import {
   Arrow,
   ArrowType, BalloonLayout, DefaultLabelStyle,
   GraphBuilder,
-  GraphComponent, IconLabelStyle, InteriorLabelModel,
-  License,
+  GraphComponent, GraphOverviewComponent, IconLabelStyle, InteriorLabelModel,
+  License, Point,
   PolylineEdgeStyle,
   Rect,
   ShapeNodeStyle, Size,
@@ -20,15 +20,20 @@ import {data} from "./data";
 })
 export class GraphComponents implements OnInit {
   data = data;
+
   ngOnInit() {
     License.value = licenseValue;
     const graphComponent = new GraphComponent("#graphComponent");
     const builder = new GraphBuilder();
+
+    //create nodes
     const nodesSource = builder.createNodesSource({
       data: data.nodes,
       id: "id",
       // labels: ["label"],
     });
+
+    //create edges
     const edgesSource = builder.createEdgesSource({
       data: data.edges,
       id: "id",
@@ -37,15 +42,19 @@ export class GraphComponents implements OnInit {
       targetId: "target"
     });
 
+    //style nodes
     nodesSource.nodeCreator.defaults.style = new ShapeNodeStyle({
       stroke: null, fill: null //for no shape node
       // shape: 'ellipse'
     });
-    //
+
+    //create text label
     const labelCreator = nodesSource.nodeCreator.createLabelBinding(data => data.label);
     labelCreator.defaults.layoutParameter = InteriorLabelModel.SOUTH
+
+    // create icon label
     const iconCreator = nodesSource.nodeCreator.createLabelBinding()
-// to avoid empty labels to be created return null for missing image URL
+    // null check
     iconCreator.textProvider = node => (node.imageUrl != null ? '' : null)
     iconCreator.styleProvider = node =>
       (new IconLabelStyle({
@@ -54,7 +63,7 @@ export class GraphComponents implements OnInit {
         iconPlacement: InteriorLabelModel.CENTER
       }))
 
-
+    // style edges
     edgesSource.edgeCreator.defaults.style = new PolylineEdgeStyle({
       stroke: "black",
       targetArrow: new Arrow({
@@ -62,10 +71,16 @@ export class GraphComponents implements OnInit {
         fill: "black"
       })
     });
+
+    // style edge label
     edgesSource.edgeCreator.defaults.labels.style = new DefaultLabelStyle({
       backgroundFill: 'white'
     })
+
+    //prepare the graph
     graphComponent.graph = builder.buildGraph();
+
+    //place the nodes
     graphComponent.graph.nodes.forEach(node => {
       // console.log(graphComponent.graph.nodes.get(7))
       const x = data.nodes.find(n => n.id === node.tag.id)?.x;
@@ -76,18 +91,22 @@ export class GraphComponents implements OnInit {
       }
     });
 
+    // customise one node style
     const nodes = graphComponent.graph.nodes.find(n => n.tag.label === 'Akshay');
     const nodeStyle = new ShapeNodeStyle({
       // fill: 'yellow',
       // shape: 'ellipse'
-      stroke:null,
-      fill:null
+      stroke: null,
+      fill: null
     })
     if (nodes) {
       graphComponent.graph.setStyle(nodes, nodeStyle)
     }
 
-
+    const overviewComponent = new GraphOverviewComponent('#overview', graphComponent);
+    overviewComponent.autoDrag = true;
+    overviewComponent.contentRect = new Rect(0,0,1000,1000)
+    overviewComponent.fitContent(true).then();
   }
 
 
