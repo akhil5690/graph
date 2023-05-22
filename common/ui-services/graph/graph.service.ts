2bda6d01edfa75cfa39a62ccd3db62019c647f31
Vandana Nayak <vandana.nayak@novaders.com>
2023-05-22 20:52:17 +0530

    api call with params for filter

--- common/ui-services/graph/graph.service.ts
+++ common/ui-services/graph/graph.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraphService {
 
+   params = new HttpParams().set('filter',false);
   constructor(private http:HttpClient) { }
 
   getGraphData():Promise<any>{
     return new Promise((resolve, reject)=>{
-      this.http.get('https://2m2ujd9wg3.execute-api.us-west-2.amazonaws.com/dev/').subscribe((data)=>{
+      this.http.get('https://2m2ujd9wg3.execute-api.us-west-2.amazonaws.com/dev/', { params:this.params }).subscribe((data)=>{
         resolve(data);
       })
     })
