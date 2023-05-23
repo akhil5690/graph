9a1a2a238c4a8f31bc3eec7f0acdf9fa247de31c
Vandana Nayak <vandana.nayak@novaders.com>
2023-05-23 17:54:58 +0530

    tabs

--- common/ui-component/right-sidebar/right-sidebar.component.ts
+++ common/ui-component/right-sidebar/right-sidebar.component.ts
@@ -4,15 +4,16 @@ import {Component, EventEmitter, Input, OnChanges, Output, ViewEncapsulation} fr
   selector: 'app-right-sidebar',
   templateUrl: './right-sidebar.component.html',
   styleUrls: ['./right-sidebar.component.scss'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
-export class RightSidebarComponent implements OnChanges{
+export class RightSidebarComponent implements OnChanges {
   items: any;
   nodeData: any;
   openPopUp = false;
 
   @Input() details: any;
   @Output() isRightSidebarOpen = new EventEmitter();
+  tab = 'details';
 
   ngOnChanges(): void {
     if (this.details) {
@@ -21,14 +22,18 @@ export class RightSidebarComponent implements OnChanges{
         Object.entries(item).map(([label, value]) => ({label, value}))
       );
       this.openPopUp = true;
-    } else {
-      this.openPopUp = false;
     }
     this.isRightSidebarOpen.emit(this.openPopUp);
   }
 
-  open(isOpen:boolean) {
+  open(isOpen: boolean) {
     this.openPopUp = isOpen && this.details;
+    this.isRightSidebarOpen.emit(this.openPopUp);
+  }
+
+  tabs(tab:string) {
+    this.openPopUp = true;
+    this.tab = tab;
     this.isRightSidebarOpen.emit(this.openPopUp);
   }
 }
