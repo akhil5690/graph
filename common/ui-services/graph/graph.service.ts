03ab63d69d7c94216c6e0d92fa99e1bb617ddc84
Vandana Nayak <vandana.nayak@novaders.com>
2023-05-23 09:45:41 +0530

    pointer for tools

--- common/ui-services/graph/graph.service.ts
+++ common/ui-services/graph/graph.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable({
@@ -6,12 +6,13 @@ import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 })
 export class GraphService {
 
-   params = new HttpParams().set('filter',false);
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  getGraphData():Promise<any>{
-    return new Promise((resolve, reject)=>{
-      this.http.get('https://2m2ujd9wg3.execute-api.us-west-2.amazonaws.com/dev/', { params:this.params }).subscribe((data)=>{
+  getGraphData(): Promise<any> {
+    let params = {filter: false};
+    return new Promise((resolve, reject) => {
+      this.http.get('https://2m2ujd9wg3.execute-api.us-west-2.amazonaws.com/dev/', {params: params}).subscribe((data) => {
         resolve(data);
       })
     })
