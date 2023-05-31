80420976775711b531602fd85ded084874777f7a
Vandana Nayak <vandana.nayak@novaders.com>
2023-05-31 09:44:11 +0530

    severity indicator

--- common/ui-component/details/details.component.ts
+++ common/ui-component/details/details.component.ts
@@ -9,4 +9,17 @@ import {Component, Input, ViewEncapsulation} from '@angular/core';
 export class DetailsComponent {
   @Input() data: any;
   headers = ['label','value']
+
+  severity(item: any, header: string) {
+    return header === 'value' && item['label']==='severity';
+  }
+
+  getStyleForSeverity(item: any, header: string) {
+    if(this.severity(item,header)){
+      switch (item['value']){
+        case 'CRITICAL': return {backgroundColor:'red'};
+      }
+    }
+    return ;
+  }
 }
