0248bf6f9265bd531e9e1449afe65bbee30aefa9
Vandana Nayak <nvandana1@gmail.com>
2023-06-21 13:01:27 +0530

    details sidebar

--- common/cym-component/details/details.component.ts
+++ common/cym-component/details/details.component.ts
@@ -7,7 +7,8 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, ViewEncapsula
   encapsulation: ViewEncapsulation.None
 })
 export class DetailsComponent implements OnChanges, OnInit {
-  data !: any
+  @Input() data !: any
+  // data:any
   headers!: string[]
 
   @Input() type: string | undefined;// editor or graph
@@ -17,7 +18,8 @@ export class DetailsComponent implements OnChanges, OnInit {
   nodeForm: { [key: string]: string } = {};
 
   ngOnChanges(): void {
-    // this.headers = Object.keys(this.data[0]);
+    this.headers = Object.keys(this.data[0]);
+    console.log(this.data,this.headers)
     this.getForm();
   }
 
@@ -43,33 +45,33 @@ export class DetailsComponent implements OnChanges, OnInit {
   }
 
   ngOnInit(): void {
-    this.data = [
-      {
-        "description": "In Django 3.2 before 3.2.19, 4.x before 4.1.9, and 4.2 before 4.2.1, it was possible to bypass " +
-          "validation when using one form field to upload multiple files. This multiple upload has never been supported " +
-          "by forms.FileField or forms.ImageField (only the last uploaded file was validated). However, Django's \"Uploading " +
-          "multiple files\" documentation suggested otherwise.",
-        "id": "arn:aws:1232342",
-        "label": "CVE-2023-31047 - django",
-        "groups": [
-          {
-            "overview": [
-              {
-                "account_id": "742352861411",
-                "severity": "CRITICAL"
-              }
-            ],
-            "affected_package": [
-              {
-                "name": "django",
-                "installed_version": "0:4.1.7 /4.2.1"
-              }
-            ]
-          }
-        ]
-      }
-    ];
-    this.headers = Object.keys(this.data[0]);
+    // this.data = [
+    //   {
+    //     "description": "In Django 3.2 before 3.2.19, 4.x before 4.1.9, and 4.2 before 4.2.1, it was possible to bypass " +
+    //       "validation when using one form field to upload multiple files. This multiple upload has never been supported " +
+    //       "by forms.FileField or forms.ImageField (only the last uploaded file was validated). However, Django's \"Uploading " +
+    //       "multiple files\" documentation suggested otherwise.",
+    //     "id": "arn:aws:1232342",
+    //     "label": "CVE-2023-31047 - django",
+    //     "groups": [
+    //       {
+    //         "overview": [
+    //           {
+    //             "account_id": "742352861411",
+    //             "severity": "CRITICAL"
+    //           }
+    //         ],
+    //         "affected_package": [
+    //           {
+    //             "name": "django",
+    //             "installed_version": "0:4.1.7 /4.2.1"
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   }
+    // ];
+    // this.headers = Object.keys(this.data[0]);
   }
 
   isSeverity(header: string) {
