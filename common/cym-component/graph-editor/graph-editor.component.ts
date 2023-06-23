8b04258efd1ca325fb1b5d7bee8bb4a8d37abcad
Vandana Nayak <nvandana1@gmail.com>
2023-06-23 11:13:41 +0530

    clean up

--- common/cym-component/graph-editor/graph-editor.component.ts
+++ common/cym-component/graph-editor/graph-editor.component.ts
@@ -43,11 +43,11 @@ import {v4 as uuidv4} from 'uuid';
   styleUrls: ['./graph-editor.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class GraphEditorComponent implements OnInit, OnChanges {
-  private graphComponent!: GraphComponent;
+export class GraphEditorComponent implements OnInit {
   @Input() data: any;
   @ViewChild('graphContainer', {static: true}) graphContainer!: ElementRef;
   @ViewChild('panel', {static: true}) panelContainer!: ElementRef;
+  private graphComponent!: GraphComponent;
   isFilterOpen: boolean = false;
   toolBarItems = [{
     toolName: 'save',
@@ -96,9 +96,6 @@ export class GraphEditorComponent implements OnInit, OnChanges {
 
   ]
   selectedItem: any;
-
-  private max = 1000000;
-  private min = 0;
   isItemClicked!: boolean;
   iGraph: any = {};
   private nodeSelection: any;
@@ -107,66 +104,106 @@ export class GraphEditorComponent implements OnInit, OnChanges {
   constructor(private cdr: ChangeDetectorRef) {
   }
 
-  ngOnChanges() {
-  }
-
   ngOnInit() {
     this.run();
   }
 
   run() {
     License.value = licenseValue;
+
+    // get the canvas for drawing graph
     const divElement = this.graphContainer.nativeElement;
     this.graphComponent = new GraphComponent(divElement);
+
+    // make the graph editable
     this.graphComponent.inputMode = new GraphEditorInputMode({
       allowGroupingOperations: true
-    })
-    this.graphComponent.graph.undoEngineEnabled = true
-    this.initTutorialDefaults(this.graphComponent.graph)
-    this.configureDragAndDrop()
+    });
+
+    // enable undoEngine
+    this.graphComponent.graph.undoEngineEnabled = true;
+
+    // set default styling for nodes, edges etc
+    this.initTutorialDefaults(this.graphComponent.graph);
+
+    // prepare drag and drop
+    this.configureDragAndDrop();
 
   }
 
   configureDragAndDrop(): void {
-    const inputMode = this.graphComponent.inputMode = new GraphEditorInputMode()
-    const nodeDropInputMode = inputMode.nodeDropInputMode
-    nodeDropInputMode.enabled = true
+
+    // get the input handler
+    const inputMode = this.graphComponent.inputMode = new GraphEditorInputMode();
+
+    // get node drag and drop input handler
+    const nodeDropInputMode = inputMode.nodeDropInputMode;
+
+    nodeDropInputMode.enabled = true;
+
+    // get group node predicate that tell that node dropped is group node
     nodeDropInputMode.isGroupNodePredicate = (draggedNode: INode): boolean =>
       draggedNode.style instanceof GroupNodeStyle
-    nodeDropInputMode.showPreview = true
+
+    // set drag and drop preview for tracing the path
+    nodeDropInputMode.showPreview = true;
+
+    // set the required input listener for nodes and edges
+    this.listeners(inputMode);
+  }
+
+  private listeners(inputMode: GraphEditorInputMode) {
+    // when a new node, edge or label is created by drag and drop or on double click
     this.nodeListener(inputMode);
+
     this.edgeListener(inputMode);
+
     this.labelListener(inputMode);
+
+    // on click of node get the sidebar open with the node properties
     this.leftClickListener(inputMode);
+
+    // start to prepare sidebar panel
     this.initializeDragAndDropPanel();
   }
 
   private leftClickListener(inputMode: GraphEditorInputMode) {
+    // node click listener which sends node or edge details to the sidebar
     inputMode.addItemLeftClickedListener((sender, evt) => {
+
       this.isItemClicked = true;
+
       this.selectedItem = evt.item instanceof IEdge || evt.item instanceof INode ? evt.item : null;
     })
   }
 
-  nodeListener(inputMode: GraphEditorInputMode) {
+  private nodeListener(inputMode: GraphEditorInputMode) {
     inputMode.addNodeCreatedListener((sender, evt) => {
-      if (evt.item.style instanceof GroupNodeStyle) {
+      const node = evt.item
+      if (node.style instanceof GroupNodeStyle) {
         console.log('is group')
       }
-      evt.item.tag = {id: uuidv4().toString(), style: evt.item.style, layout: evt.item.layout};
-      this.graphComponent.graph.nodes.append(evt.item);
+
+      // set node tag for creating json
+      node.tag = {id: uuidv4().toString(), style: node.style, layout: node.layout};
+
+      // add the new node to the graph
+      this.graphComponent.graph.nodes.append(node);
     });
   }
 
   labelListener(inputMode: GraphEditorInputMode) {
+    // when new label is created
     inputMode.addLabelAddedListener(this.getLabelListner);
 
     inputMode.addLabelTextChangedListener(this.getLabelListner);
   }
 
   getLabelListner = (sender: any, evt: { item: any; }) => {
+    // label handler
     const label = evt.item;
     const owner = label.owner;
+
     if (owner instanceof INode) {
       console.log(this.validateLabel(label.text, 'node'));
       owner.tag = {
@@ -175,7 +212,10 @@ export class GraphEditorComponent implements OnInit, OnChanges {
         style: owner.tag.style,
         layout: owner.tag.layout
       };
+
+      //when the node label is change, change the source and target of the respective edge
       this.replaceEdgeTag(owner.tag.id, label.text, owner.tag.label)
+
     } else if (owner instanceof IEdge) {
       owner.tag = {
         id: owner.tag.id,
@@ -188,17 +228,22 @@ export class GraphEditorComponent implements OnInit, OnChanges {
         layout: owner.tag.layout
       };
     }
+
     this.save();
     this.createGraph(this.iGraph)
   }
 
   private replaceEdgeTag(id: string, label: string, oldLabel: string) {
+    //when the node label is change, change the source and target of the respective edge
+
     this.graphComponent.graph.edges.forEach((edge) => {
+
       if (edge.tag.sourceLabel === id || edge.tag.sourceLabel === oldLabel) {
         edge.tag.sourceLabel = label
       } else if (edge.tag.targetLabel === id || edge.tag.targetLabel === oldLabel) {
         edge.tag.targetLabel = label
       }
+
     });
 
   }
@@ -206,9 +251,11 @@ export class GraphEditorComponent implements OnInit, OnChanges {
 
   edgeListener(inputMode: GraphEditorInputMode) {
     inputMode.createEdgeInputMode.addEdgeCreatedListener((sender, evt) => {
+
       const edge = evt.item;
       const sourceNode = edge.sourceNode;
       const targetNode = edge.targetNode;
+
       edge.tag = {
         id: uuidv4().toString(),
         source: sourceNode?.tag?.id,
@@ -217,12 +264,16 @@ export class GraphEditorComponent implements OnInit, OnChanges {
         targetLabel: targetNode?.tag?.label ? targetNode?.tag?.label : targetNode?.tag?.id,
         style: edge.style,
       };
+
       this.graphComponent.graph.edges.append(edge);
     })
   }
 
   initializeDragAndDropPanel(): void {
-    const panel = this.panelContainer.nativeElement
+    // get the div for panel
+    const panel = this.panelContainer.nativeElement;
+
+    // set the node styles
     const defaultNode = this.graphComponent.graph.nodeDefaults.style
     const ellipse = createShapeNodeStyle(ShapeNodeShape.ELLIPSE);
     const rectangle = createShapeNodeStyle(ShapeNodeShape.RECTANGLE);
@@ -234,13 +285,19 @@ export class GraphEditorComponent implements OnInit, OnChanges {
     // const icon = createIconNode('assets/image/edit.svg')
     const defaultGroupNodeStyle = this.graphComponent.graph.groupNodeDefaults.style;
     const newGroup = createDemoGroupStyle({colorSetName: 'demo-palette-23', foldingEnabled: true})
+
+    // create an array of all node styles
     const nodeStyles = [defaultNode, ellipse, rectangle, fatArrow, fatArrow2, hexagon, hexagon2, diamond, defaultGroupNodeStyle, newGroup]
+
+    // create visual images for the nodes for panel
     nodeStyles.forEach((style: any): void => {
       this.addNodeVisual(style, panel)
     })
   }
 
   createNodeVisual(style: any): string {
+
+    // get svg of the style for creating source url for image
     const exportComponent = new GraphComponent()
     const exportGraph = exportComponent.graph
     exportGraph.createNode(new Rect(0, 0, 40, 40), style)
@@ -249,15 +306,20 @@ export class GraphEditorComponent implements OnInit, OnChanges {
     const svg = svgExport.exportSvg(exportComponent)
     const svgString = SvgExport.exportSvgString(svg)
     return SvgExport.encodeSvgDataUrl(svgString)
+
   }
 
   addNodeVisual(style: any, panel: Element): void {
+    // set the div for image
     const div = document.createElement('div')
-    div.setAttribute('style', 'width: 40px; height: 40px; margin: 10px auto; cursor: grab;')
+    div.setAttribute('style', 'width: 40px; height: 40px; margin: 10px auto; cursor: grab;');
+
+    // set image
     const img = document.createElement('img')
     img.setAttribute('style', 'width: auto; height: auto;')
     img.setAttribute('src', this.createNodeVisual(style))
 
+    // initialise drag handler
     const startDrag = (): void => {
       const simpleNode = new SimpleNode()
       simpleNode.layout = new Rect(0, 0, 40, 40)
@@ -296,13 +358,18 @@ export class GraphEditorComponent implements OnInit, OnChanges {
         startDrag()
         event.preventDefault()
       },
-    )
+    );
+    // add the image to the div
     div.appendChild(img)
+    // add div to the panel
     panel.appendChild(div)
   }
 
   initTutorialDefaults(graph: IGraph): void {
+
+    // set default styling for nodes and edges
     initDemoStyles(graph)
+
     graph.groupNodeDefaults.style = new GroupNodeStyle({
       tabFill: '#46a8d5',
       tabPosition: 'top-leading',
@@ -312,12 +379,16 @@ export class GraphEditorComponent implements OnInit, OnChanges {
       horizontalTextAlignment: 'left',
       textFill: '#eee'
     })
+
     graph.groupNodeDefaults.labels.layoutParameter =
       new GroupNodeLabelModel().createDefaultParameter()
+
     graph.nodeDefaults.size = new Size(40, 40)
+
     graph.nodeDefaults.labels.layoutParameter = new ExteriorLabelModel({
       insets: 5
     }).createParameter('south')
+
     graph.edgeDefaults.labels.layoutParameter = new EdgePathLabelModel({
       distance: 5,
       autoRotation: true
@@ -325,7 +396,10 @@ export class GraphEditorComponent implements OnInit, OnChanges {
   }
 
   createGraph(initGraph: any): void {
+
+    // get the graph builder to create graph from json ie; initGraph
     const builder = new GraphBuilder()
+
     const sourceNode = builder.createNodesSource({
       data: initGraph.nodes, id: "id", labels: ['label'], style: "style", layout: "layout"
     });
@@ -333,6 +407,7 @@ export class GraphEditorComponent implements OnInit, OnChanges {
     const edgeNode = builder.createEdgesSource({
       data: initGraph.edges, id: "id", labels: ['label'], sourceId: "source", targetId: "target", style: "style"
     })
+
     edgeNode.edgeCreator.defaults.labels.style = new DefaultLabelStyle({
       backgroundFill: 'white',
       textSize: 10
@@ -341,15 +416,19 @@ export class GraphEditorComponent implements OnInit, OnChanges {
     const labelModel = new EdgePathLabelModel({distance: 50});
 
     edgeNode.edgeCreator.defaults.labels.layoutParameter = labelModel.createDefaultParameter();
+
     this.graphComponent.graph = builder.buildGraph();
+
     this.initTutorialDefaults(this.graphComponent.graph)
   }
 
   setFrame(isFilterOpen: boolean) {
+    // frame for sidebar and the graph
     this.isFilterOpen = isFilterOpen;
   }
 
   clickEvent(tool: { icon: string; toolName: string }) {
+    // tools
     if (this.graphComponent) {
       switch (tool.toolName) {
         case 'save':
@@ -388,20 +467,27 @@ export class GraphEditorComponent implements OnInit, OnChanges {
   }
 
   save() {
+
+    // create json
     const jsonGraph: { nodes: any[], edges: any[] } = {
       nodes: [],
       edges: []
     };
+
     this.graphComponent.graph.nodes.forEach((node) => {
+
       const jsonNode = {
         id: node?.tag?.id,
         label: node?.tag?.label,
         style: node.tag.style, layout: node.tag.layout,
-        properties:node.tag?.properties
+        properties: node.tag?.properties
       };
+
       jsonGraph.nodes.push(jsonNode);
     });
+
     this.graphComponent.graph.edges.forEach((edge) => {
+
       const jsonEdge = {
         id: edge?.tag?.id,
         source: edge?.tag?.source,
@@ -412,19 +498,28 @@ export class GraphEditorComponent implements OnInit, OnChanges {
         style: edge.tag.style
         // Include other edge properties as needed
       };
+
       jsonGraph.edges.push(jsonEdge);
     });
-    console.log(jsonGraph)
+
+    console.log(jsonGraph);
+
+    // save the json in a variable
     this.iGraph = jsonGraph;
   }
 
   changeEdgeNode(property: any) {
+    // setting the manipulated graph properties to the graph
     if (property.source) {
+
       this.graphComponent.graph.edges.forEach((data) => {
+
         const source = this.graphComponent.graph.nodes.find((node) =>
           (node.tag.label === property.sourceLabel || node.tag.id === property.sourceLabel))?.tag.id;
+
         const target = this.graphComponent.graph.nodes.find((node) =>
           (node.tag.label === property.targetLabel || node.tag.id === property.targetLabel))?.tag.id;
+
         if (data.tag.id === property.id) {
           data.tag = {
             id: data.tag.id,
@@ -439,16 +534,18 @@ export class GraphEditorComponent implements OnInit, OnChanges {
       })
     } else {
       this.graphComponent.graph.nodes.forEach((data) => {
-        console.log(property)
         if (data.tag.id === property.id) {
+
           const oldLabel = data.tag.label
+
           data.tag = {
             id: data.tag.id,
             label: !this.validateLabel(property.label, 'node') ? property.label : null,
             style: data.tag.style,
             layout: data.tag.layout,
-            properties:property.properties
+            properties: property.properties
           }
+
           this.replaceEdgeTag(data.tag.id, data.tag.label, oldLabel)
 
         }
@@ -460,18 +557,24 @@ export class GraphEditorComponent implements OnInit, OnChanges {
   }
 
   private validateLabel(label: string, type: string) {
+    // label shouldn't be same
     if (type === 'edge') {
       return this.graphComponent.graph.edges.find((edge) => edge.tag.label === label)
     } else {
       return this.graphComponent.graph.nodes.find((node) => node.tag.label === label)
     }
+
   }
 
   private copy() {
+    // store the selected nodes and edges
     this.nodeSelection = this.graphComponent.selection.selectedNodes.toList().map((node) => node.tag.id);
     this.edgeSelection = this.graphComponent.selection.selectedEdges.toList().map((edge) => edge.tag.id);
+
+    // copy the items
     ICommand.COPY.execute(null, this.graphComponent);
     this.graphComponent.clipboard.fromClipboardCopier.addNodeCopiedListener((sender, evt) => {
+      // shift the position of the copied node 5px from the original node
       this.graphComponent.graph.setNodeLayout(evt.copy, new Rect(evt.copy.layout.x + 5, evt.copy.layout.y, evt.copy.layout.width, evt.copy.layout.height))
       evt.copy.tag = {id: uuidv4(), label: undefined, style: evt.original.style, layout: evt.copy.layout};
 
@@ -487,12 +590,18 @@ export class GraphEditorComponent implements OnInit, OnChanges {
       };
     })
 
+  // Note: selection is lost here
+
   }
 
   private paste() {
+
     ICommand.PASTE.execute(null, this.graphComponent);
+
     this.save();
     this.createGraph(this.iGraph);
+
+    // regain the selection back
     this.graphComponent.graph.nodes.forEach((node) => {
       // this.graphComponent.selection.setSelected(node, true);
       if (this.nodeSelection.includes(node.tag.id)) {
@@ -500,4 +609,6 @@ export class GraphEditorComponent implements OnInit, OnChanges {
       }
     })
   }
+
+
 }
