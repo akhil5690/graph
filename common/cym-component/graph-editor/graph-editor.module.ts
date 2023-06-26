7403ce9ba67cc1549a3608479eb3df5078b8e71b
Vandana Nayak <nvandana1@gmail.com>
2023-06-26 19:18:00 +0530

    overview

--- common/cym-component/graph-editor/graph-editor.module.ts
+++ common/cym-component/graph-editor/graph-editor.module.ts
@@ -4,9 +4,10 @@ import {GraphEditorComponent} from "./graph-editor.component";
 import {RightSidebarModule} from "../right-sidebar/right-sidebar.module";
 import {DropdownModule} from "primeng/dropdown";
 import {FormsModule} from "@angular/forms";
+import {OverviewComponent} from "../overview/overview.component";
 
 @NgModule({
-  declarations: [GraphEditorComponent],
+  declarations: [GraphEditorComponent, OverviewComponent],
   imports: [
     CommonModule,
     RightSidebarModule,
