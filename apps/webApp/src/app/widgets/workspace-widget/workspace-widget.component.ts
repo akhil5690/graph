52d5e2b2bb1c2a8c3d54ada5217c8a3a4285eebf
AKhilesh_n5690 <akhilesh.n@novaders.com>
2023-06-30 23:07:04 +0530

    workspace changes

--- apps/webApp/src/app/widgets/workspace-widget/workspace-widget.component.ts
+++ apps/webApp/src/app/widgets/workspace-widget/workspace-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'cym-workspace-widget',
@@ -10,7 +10,7 @@ export class WorkspaceWidgetComponent {
     name: "Observables",
     cardDivision: [{
       image: "assets/image/find_workspace.svg",
-      subHeader2:"No new Observables"
+      subHeader2: "No new Observables"
     },]
   },
     {
@@ -18,22 +18,25 @@ export class WorkspaceWidgetComponent {
       cardDivision: [{
         image: "assets/image/clipboard_workspace.svg",
 
-        subHeader2:"You don't have any new tasks"
+        subHeader2: "You don't have any new tasks"
       },]
     },
     {
       name: "Shared with Me",
       cardDivision: [{
-        image:  "assets/image/folder_workspace.svg",
+        image: "assets/image/folder_workspace.svg",
         subHeader2: "No files shared with you yet"
       }],
     },
-    {
-      name: null,
-      cardDivision: [{
-        image:  "assets/image/Layer-6.svg",
-        subHeader1: "No Workspace created or shared yet",
-        subHeader2: "Create your first workspace here!"
-      }],
-    }]
+  ]
+
+  workspace1: any = [{
+    name: null,
+    cardDivision: [{
+      image: "assets/image/Layer-6.svg",
+      subHeader1: "No Workspace created or shared yet",
+      subHeader2: "Create your first workspace here!"
+    }],
+  }]
 }
+
