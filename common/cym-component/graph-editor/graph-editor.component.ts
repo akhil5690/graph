e965246302de82a837f9684526b57fdfcd439658
Vandana Nayak <nvandana1@gmail.com>
2023-07-07 10:57:35 +0530

    error on edge label highlight fix

--- common/cym-component/graph-editor/graph-editor.component.ts
+++ common/cym-component/graph-editor/graph-editor.component.ts
@@ -328,7 +328,22 @@ export class GraphEditorComponent implements OnInit {
           if (newItem instanceof INode) {
             // and if it's a node, we highlight all adjacent edges, too
             for (const edge of this.graphComponent.graph.edgesAt(newItem)) {
+
+              const labelStyle = new DefaultLabelStyle({
+                backgroundFill: 'white',
+                textSize: 10,
+                verticalTextAlignment:'center',
+                horizontalTextAlignment:'center'
+              });
+              const labelStyleHighlight = new LabelStyleDecorationInstaller({
+                labelStyle,
+                zoomPolicy:StyleDecorationZoomPolicy.WORLD_COORDINATES
+              })
+              decorator.labelDecorator.highlightDecorator.setImplementation(labelStyleHighlight)
               styleHighlight?.addHighlight(edge)
+              if(edge.tag.label){
+                styleHighlight.addHighlight(edge?.labels?.get(0))
+              }
             }
           } else if (newItem instanceof IEdge) {
             // if it's an edge - we highlight the adjacent nodes
