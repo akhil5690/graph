9cf33f3640ab00c797e0e3fa7ad60fd1d0c13ea9
Vandana Nayak <nvandana1@gmail.com>
2023-07-17 18:14:13 +0530

    using cym-div

--- apps/webApp/src/app/widgets/workspace-widget/workspace-widget.component.ts
+++ apps/webApp/src/app/widgets/workspace-widget/workspace-widget.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Router} from "@angular/router";
 import {workSpaceService} from "../../../../../../common/cym-services/workspace/workspace.service";
 
 @Component({
   selector: 'cym-workspace-widget',
   templateUrl: './workspace-widget.component.html',
-  styleUrls: ['./workspace-widget.component.scss']
+  styleUrls: ['./workspace-widget.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class WorkspaceWidgetComponent implements OnInit {
   workspaceRes: any;
